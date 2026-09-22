bfb958c5a539baeecb80ce17df0ab59a603e071a
-get <officialmdmahadi@gmail.com>
2026-09-22 14:58:42 +0600

    fix(home): remove Book Now booking bar form from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/home/Hero";
-import BookingBar from "@/components/home/BookingBar";
 import PastGigs from "@/components/home/PastGigs";
 import MeetTheDjs from "@/components/home/MeetTheDjs";
 import EventsWeCover from "@/components/home/EventsWeCover";
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <BookingBar />
       <PastGigs />
       <MeetTheDjs />
       <EventsWeCover />
